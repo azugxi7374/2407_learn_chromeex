a1770ef35d33d7d1bd743ec3d441081fba636e47
ixguza <rtk0253@gmail.com>
2024-08-06 23:32:26 +0900

    0805

--- background.js
+++ background.js
@@ -3,3 +3,14 @@ chrome.runtime.onInstalled.addListener(() => {
         text: "OFF",
     });
 });
+
+console.log("here is background.js")
+chrome.action.onClicked.addListener(async (tab) => {
+    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+
+    await chrome.action.setBadgeText({
+        tabId: tab.id,
+        text: nextState,
+    });
+})
